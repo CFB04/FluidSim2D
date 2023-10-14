e9093cd7f90f05917869bb0704a48b74c70df5ed
Duncan Joly <duncanjoly@protonmail.com>
2023-10-14 04:28:58 -0500

    Remove unused import and make class variable final

--- src/main/java/cfbastian/fluidsim2d/simulation/sph/SPHParticle.java
+++ src/main/java/cfbastian/fluidsim2d/simulation/sph/SPHParticle.java
@@ -1,10 +1,9 @@
 package cfbastian.fluidsim2d.simulation.sph;
 
 import cfbastian.fluidsim2d.simulation.Particle;
-import cfbastian.fluidsim2d.simulation.util.Bounds;
 
 public class SPHParticle extends Particle {
-    private float r;
+    private final float r;
 
     public SPHParticle(float x, float y, float dx, float dy, int col, float r) {
         super(x, y, dx, dy, col);
